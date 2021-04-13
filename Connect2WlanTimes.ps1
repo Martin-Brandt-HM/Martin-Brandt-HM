f725d2db75b69b5f61f783adb56e91f2800e828d
Martin-Brandt-HM <63795884+Martin-Brandt-HM@users.noreply.github.com>
2021-04-13 19:22:11 +0200

    Add files via upload

---
+++ Connect2WlanTimes.ps1
@@ -0,0 +1,27 @@
+﻿function RunUntilConnected
+{
+    $Connected = Test-NetConnection -Computername google.nl -InformationLevel Quiet -WarningAction SilentlyContinue
+    if ($Connected -ne "True") 
+        {
+            RunUntilConnected
+        }
+}
+
+
+function testconnection
+{
+    netsh wlan connect name = "eduroam"
+    $stopwatch =  [system.diagnostics.stopwatch]::StartNew()
+    RunUntilConnected
+    $ConnectTime = $stopwatch.ElapsedMilliseconds
+    $TimeStamp = Get-Date -format "dd-MMM-yyyy HH:mm"
+    $ExportTime = $TimeStamp + ";" + $ConnectTime
+    Write-Host "Last Connection Time"
+    Write-Host $ExportTime
+    $ExportTime | out-file –append –filepath $PSScriptRoot\ConnectionTimes.csv
+    netsh wlan disconnect
+    testconnection
+}
+
+testconnection
+
